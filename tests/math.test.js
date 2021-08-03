5b92554d12286263b4595f431ceb0ae3c73fd044
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-03 09:42:21 -0230

    FIrst commit

---
+++ tests/math.test.js
@@ -0,0 +1,20 @@
+test('Hello World!', () => {
+
+})
+
+test('This should fail', () => {
+	throw new Error('Failure!')
+})
+
+/***
+ *  Why Test?
+ *  
+ *  - Saves time
+ *  - Creates reliable software
+ *  - Gives flexibility to developers
+ *   - Refactoring
+ *   - Collaboration
+ *   - Profiling
+ *  - Peace of Mind
+ * 
+ */
